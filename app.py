8892673c509c1493c7e9e895439107ac9637c1be
Harshit Somani <harshitsomani2005@gmail.com>
2025-12-19 15:12:17 +0530

    Upgrade Gemini SDK and fix model

--- app.py
+++ app.py
@@ -5,7 +5,8 @@ import os
 
 # ---------------- GEMINI CONFIG ---------------- #
 genai.configure(api_key=os.environ.get("GEMINI_API_KEY"))
-model = genai.GenerativeModel("gemini-1.5-flash")
+model = genai.GenerativeModel("models/gemini-1.5-flash")
+
 
 # ---------------- APP CONFIG ---------------- #
 app = Flask(__name__)
